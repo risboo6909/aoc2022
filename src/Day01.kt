7410308a1213057a110d2c1a2c99ad0671c3dba9
risboo6909 <ttyv00@gmail.com>
2022-12-01 13:19:51 +0300

    First day solved in Kotlin (also my first code in Kotlin)

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,53 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxSoFar: Int = -1
+        var groupSum = 0
+
+        for (line: String in input) {
+            val parsedInt = line.toIntOrNull()
+            if (parsedInt != null) {
+                groupSum += parsedInt
+            } else {
+                // group separator
+                if (groupSum > maxSoFar) {
+                    maxSoFar = groupSum
+                }
+                groupSum = 0
+            }
+        }
+
+        if (groupSum > maxSoFar) {
+            maxSoFar = groupSum
+        }
+
+        return maxSoFar
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var groupSum = 0
+        var groupSums = mutableListOf<Int>()
+
+        for (line: String in input) {
+            val parsedInt = line.toIntOrNull()
+            if (parsedInt != null) {
+                groupSum += parsedInt
+            } else {
+                // group separator
+                groupSums.add(groupSum)
+                groupSum = 0
+            }
+        }
+
+        groupSums.add(groupSum)
+
+        val tmp = groupSums.sorted().reversed()
+        return tmp[0] + tmp[1] + tmp[2]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
